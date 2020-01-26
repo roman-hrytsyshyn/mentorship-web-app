e46204e592d9524878f16fbd89e7876bc89a0169
Roman Hrytsyshyn <roman.hrytsyshyn@cgm.com>
2020-01-26 13:48:29 +0200

    Added CORS filter

--- src/main/java/com/softserve/mentorship/webapp/web/config/WebConfig.java
+++ src/main/java/com/softserve/mentorship/webapp/web/config/WebConfig.java
@@ -2,11 +2,17 @@ package com.softserve.mentorship.webapp.web.config;
 
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
 
 @EnableWebMvc
 @Configuration
 @ComponentScan(basePackages = {"com.softserve.mentorship.webapp.web"})
-public class WebConfig {
+public class WebConfig extends WebMvcConfigurerAdapter {
 
+    @Override
+    public void addCorsMappings(CorsRegistry registry) {
+        registry.addMapping("/**");
+    }
 }
